45c8474fe0d1ed448c5c4db3ed869cab46c861e7
Bonjov Hima <bonjovhima@hotmail.de>
2025-07-08 01:26:50 +0200

    setup frontend and clerk authetification & added backend dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
 import { Button } from "./components/ui/button"
 
 function App() { 
 
   return (
     <>
-      <h1 className="text-red-500 text-5xl">Hello</h1>
-      <Button variant="secondary">This a test button</Button>
+      <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
     </>
   )
 }
