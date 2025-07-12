32cf0ce83726432f41d1c0e14bcd1def8f38293c
Bonjov Hima <bonjovhima@hotmail.de>
2025-07-12 21:30:54 +0200

    added featured, made for you and trending songs to the music store

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -8,9 +8,15 @@ interface MusicStore {
 	isLoading: boolean;
 	error: string | null;
   currentAlbum: Album | null,
+  featuredSongs: Song[];
+	madeForYouSongs: Song[];
+	trendingSongs: Song[];
 
 	fetchAlbums: () => Promise<void>;
   fetchAlbumById: (id: string) => Promise<void>;
+  fetchFeaturedSongs: () => Promise<void>;
+	fetchMadeForYouSongs: () => Promise<void>;
+	fetchTrendingSongs: () => Promise<void>;
 	
 }
 
@@ -21,6 +27,9 @@ export const useMusicStore = create<MusicStore>((set) => ({
   isLoading: false,
   error: null,
   currentAlbum: null,
+  madeForYouSongs: [],
+	featuredSongs: [],
+	trendingSongs: [],
   
 //* Fetches all albums from the API
   fetchAlbums: async () => {
@@ -52,5 +61,43 @@ export const useMusicStore = create<MusicStore>((set) => ({
     } finally {
       set({isLoading: false})
     }
-  }
+  },
+
+  fetchFeaturedSongs: async() => {
+    set({isLoading: true, error: null})
+    try {
+      const response = await axiosInstance.get("songs/featured")
+      set({featuredSongs: response.data})
+    } catch (error:any) {
+      set({error: error.response.data.message})
+    } finally {
+      set({isLoading: false})
+    }
+  },
+
+  fetchMadeForYouSongs: async () => {
+    set({isLoading: true, error: null})
+
+    try {
+      const response = await axiosInstance.get("songs/made-for-you")
+      set({madeForYouSongs: response.data})
+    } catch (error:any) {
+      set({error: error.response.data.message})
+    } finally {
+      set({isLoading: false})
+    }
+  },
+
+  fetchTrendingSongs: async () => {
+    set({isLoading: true, error: null})
+    try {
+      const response = await axiosInstance.get("songs/trending")
+      set({featuredSongs: response.data})
+    } catch (error: any) {
+      set({error: error.response.data.message})
+    } finally {
+      set({isLoading: false})
+    }
+  },
+
 }));
